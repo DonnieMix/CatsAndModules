0b8c05c48d090dbe31dfbed5e15afd33fa8b6d45
DonnieMix <kyrylo.derkach@ukma.edu.ua>
2023-05-26 21:41:34 +0300

    Initial Commit

---
+++ CatsAndModules/CatsAndModulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAndModulesApp.swift
+//  CatsAndModules
+//
+//  Created by Kyrylo Derkach on 26.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CatsAndModulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
